f3660d85d7b3fa6f3d3d2700160fd42dd8d4e72f
Steven_Santamorena <stevenasanta@example.com>
2024-02-16 22:58:02 -0500

    Add temporary benchmarking command

--- cogs/scanner.py
+++ cogs/scanner.py
@@ -5,6 +5,7 @@ import utility.database as DB
 from bot import MiniSigma
 from tqdm import tqdm
 import logging
+from typing import AsyncIterator
 import time
 
 logger = logging.getLogger("client.scanner")
@@ -121,6 +122,69 @@ class Scanner(commands.Cog):
         await self.fill_names(None)
 
         await prog_message.edit(content=prog_string + "✅ Scan done! Results recorded in log.")
+
+    async def get_history_list(self, channel: discord.TextChannel) -> list[discord.Message]:
+        start_pt = channel.created_at
+        end_pt = datetime.datetime.now()
+
+        hist: list[discord.Message] = [message async for message in channel.history(limit=None, after=start_pt, before=end_pt)]
+
+        return hist
+
+    @commands.command()
+    async def benchmark_scan(self, ctx: commands.Context, channel: discord.TextChannel = None):
+        '''Benchmark scanning one channel'''
+        channel = ctx.channel if channel is None else channel
+        logger.info(f"{ctx.author.name} issued !benchmark_scan, {channel}, ({ctx.channel})")
+
+        start_time = time.perf_counter()
+        hist: list[discord.Message] = await self.get_history_list(channel)
+        flatten_time = time.perf_counter() - start_time
+        logger.info(f"Flattening {len(hist)} messages done in {flatten_time:.2f} seconds. First poster: {hist[0].author.name}")
+        await ctx.reply(f"Flattening: {flatten_time:.2f} seconds")
+
+        start_time = time.perf_counter()
+        (upvote, downvote) = self.db.get_emojis(channel.guild.id)
+        for message in hist:
+            for reaction in message.reactions:
+                if str(reaction.emoji) not in (upvote, downvote):
+                    continue
+
+                async for voter in reaction.users():
+                    if voter.id == message.author.id:
+                        continue
+                    self.db.add_reaction(voter.id, message.author.id, message.id, channel.id, channel.guild.id, 1 if str(reaction.emoji) == upvote else -1, message.created_at.strftime('%Y-%m-%d %H:%M:%S'))
+        reaction_enter_time = time.perf_counter() - start_time
+        logger.info(f"Entering reactions for {len(hist)} messages done in {reaction_enter_time:.2f} seconds.")
+        await ctx.reply(f"Entering reactions: {reaction_enter_time:.2f} seconds")
+
+        start_time = time.perf_counter()
+        upvoted_msg_count, downvoted_msg_count = 0, 0
+        for message in hist:
+            for reaction in message.reactions:
+                emoji = str(reaction.emoji)
+                if emoji not in (upvote, downvote):
+                    continue
+
+                if emoji == upvote:
+                    upvoted_msg_count += 1
+                    async for voter in reaction.users():
+                        if voter.id == message.author.id:
+                            continue
+                        self.db.upvote_user(message.author.id, 1, voter.id)
+
+                elif emoji == downvote:
+                    downvoted_msg_count += 1
+                    async for voter in reaction.users():
+                        if voter.id == message.author.id:
+                            continue
+                        self.db.downvote_user(message.author.id, 1, voter.id)
+
+                else:
+                    raise Exception("Emoji was in vote_strings, but didn't match either case. How did you get here?")
+        scan_time = time.perf_counter() - start_time
+        logger.info(f"Scanning {len(hist)} messages done in {scan_time:.2f} seconds. Upvoted: {upvoted_msg_count}, Downvoted: {downvoted_msg_count}")
+        await ctx.reply(f"Scanning: {scan_time:.2f} seconds")
 
 async def setup(client: MiniSigma):
     await client.add_cog(Scanner(client))
