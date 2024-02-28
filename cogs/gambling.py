3209880949ff6dd757d3aac0df6289ff40ac8335
Steven_Santamorena <stevenasanta@example.com>
2024-02-28 12:11:23 -0500

    Rework blackjack, reformat embed

--- cogs/gambling.py
+++ cogs/gambling.py
@@ -7,120 +7,124 @@ import random
 
 logger = logging.getLogger("client.gambling")
 
-class card:
-    def __init__(self, suit, value):
+# Blackjack game
+
+class Card:
+    '''Represents a playing card with a suit and value.'''
+    def __init__(self, suit: str, value: int):
         self.suit = suit
         self.value = value
+        self.down = False
 
-    def __str__(self):
-        return f"[{self.value} {self.suit}]"
+    @property
+    def symbol(self) -> str:
+        if self.value <= 10: return str(self.value)
+        return { 11: 'J', 12: 'Q', 13: 'K', 14: 'A' }[self.value]
+
+    def __str__(self) -> str:
+        return f"[{self.symbol} {self.suit}]" if not self.down else "[? ?]"
     
-class deck:
-    def __init__(self):
-        self.cards: list[card] = []
-        self.build()
+class Deck:
+    '''Represents a deck of playing cards.'''
+    def __init__(self, size: int = 1):
+        self.cards: list[Card] = []
+        for _ in range(size):
+            self.cards.extend([Card(suit, value) for suit in ["♠", "♣", "♦", "♥"] for value in range(2, 15)])
         self.shuffle()
-
-    def build(self):
-        for suit in ["♠", "♣", "♦", "♥"]:
-            for value in ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
-                self.cards.append(card(suit, value))
     
-    def shuffle(self):
+    def shuffle(self) -> None:
+        # Fisher-Yates shuffle
         for i in range(len(self.cards) - 1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
     
-    def drawCard(self):
+    def drawCard(self) -> Card:
         return self.cards.pop()
     
-class hand:
-    def __init__(self, deck: deck):
-        self.cards: list[card] = []
-        self.addCard(deck.drawCard())
-        self.addCard(deck.drawCard())
-    
-    def addCard(self, card: card):
-        self.cards.append(card)
-    
-    def handValue(self) -> int:
-        total = sum(11 if card.value == 'A' else 10 if card.value in ['J', 'Q', 'K'] else int(card.value) for card in self.cards)
-        aces = sum(1 for card in self.cards if card.value == 'A')
+class BlackjackInactiveView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+
+    @discord.ui.button(label="Go Again!", style=discord.ButtonStyle.secondary, emoji="🔄")
+    async def start(self, interaction: discord.Interaction, _: discord.ui.Button):
+        self.stop()
+        view = BlackjackView()
+        await view.update(interaction)
+
+class BlackjackView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.deck = Deck()
+        self.playerHand = [self.deck.drawCard(), self.deck.drawCard()]
+        self.dealerHand = [self.deck.drawCard(), self.deck.drawCard()]
+        self.dealerHand[1].down = True
+
+        self.embed = discord.Embed(title="BlackJack", color=discord.Color.green())
+        self.embed.add_field(name="Dealer's Hand:", value=f"```{self.handStr(self.dealerHand)}```", inline=False)
+        self.embed.add_field(name="Player's Hand:", value=f"```{self.handStr(self.playerHand)}```", inline=False)
+
+    def update_hands(self):
+        self.embed.set_field_at(0, name="Dealer's Hand:", value=f"```{self.handStr(self.dealerHand)}```", inline=False)
+        self.embed.set_field_at(1, name="Player's Hand:", value=f"```{self.handStr(self.playerHand)}```", inline=False)
+
+    async def send(self, interaction: discord.Interaction):
+        await interaction.response.send_message(embed=self.embed, view=self)
+
+    async def update(self, interaction: discord.Interaction):
+        await interaction.response.edit_message(embed=self.embed, view=self)
+
+    @staticmethod
+    def handValue(hand: list[Card]) -> int:
+        hand = [card for card in hand if not card.down]
+        total = sum(11 if card.symbol == 'A' else 10 if card.symbol in 'JQK' else int(card.value) for card in hand)
+        aces = sum(1 for card in hand if card.symbol == 'A')
         while total > 21 and aces:
             total -= 10
             aces -= 1
         return total
     
-    def isBlackjack(self) -> bool:
-        return self.handValue() == 21
-    
-    def isBust(self) -> bool:
-        return self.handValue() > 21
-    
-    def __str__(self, secret: bool = False):
+    @staticmethod
+    def handStr(hand: list[Card]) -> str:
         hand_str = ""
-        if secret:
-            hand_str += str(self.cards[0]) + " [? ?]"
-        else:
-            for card in self.cards:
-                hand_str += str(card) + " "
-        hand_str += f"\nTotal: {self.handValue()}"
+        for card in hand:
+            hand_str += str(card) + " "
+        hand_str += f"\nTotal: {BlackjackView.handValue(hand)}"
         return hand_str
 
-class BlackjackView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.deck = deck()
-        self.playerHand = hand(self.deck)
-        self.dealerHand = hand(self.deck)
-
-        self.embed = discord.Embed(title="BlackJack", color=discord.Color.green())
-        self.embed.add_field(name="Dealer's Hand:", value=self.dealerHand)
-        self.embed.add_field(name="Player's Hand:", value=self.playerHand)
-
-    def update_hands(self):
-        self.embed.set_field_at(0, name="Dealer's Hand:", value=self.dealerHand)
-        self.embed.set_field_at(1, name="Player's Hand:", value=self.playerHand)
+    @staticmethod
+    def endCheck(hand: list[Card]) -> bool:
+        return BlackjackView.handValue(hand) >= 21
     
-    def pickWinner(self) -> str:
-        if self.playerHand.isBlackjack() and not self.dealerHand.isBlackjack():
-            return "BlackJack! Player wins!"
-        elif self.dealerHand.isBlackjack() and not self.playerHand.isBlackjack():
-            return "BlackJack! Dealer wins!"
-        elif self.playerHand.isBust():
-            return "Player bust! Dealer wins!"
-        elif self.dealerHand.isBust():
-            return "Dealer bust! Player wins!"
-        elif self.playerHand.handValue() > self.dealerHand.handValue():
-            return "Player wins!"
-        elif self.playerHand.handValue() < self.dealerHand.handValue():
-            return "Dealer wins!"
-        else:
-            return "It's a tie!"
-
-    async def send(self, interaction: discord.Interaction):
-            await interaction.response.send_message(embed=self.embed, view=self)
+    def winStr(self) -> str:
+        player = self.handValue(self.playerHand)
+        dealer = self.handValue(self.dealerHand)
+        if player > 21: return "Player busts, dealer wins!"
+        if dealer > 21: return "Dealer busts, player wins!"
+        if player == dealer: return "It's a tie!"
+        if player > dealer: return "Player wins!"
+        return "Dealer wins!"
 
     async def endGame(self, interaction: discord.Interaction):
         self.update_hands()
-        self.embed.add_field(name="Result:", value=self.pickWinner(), inline=False)
-        await interaction.response.edit_message(embed=self.embed, view=None)
+        self.embed.add_field(name="Result:", value=self.winStr(), inline=False)
+        await interaction.response.edit_message(embed=self.embed, view=BlackjackInactiveView())
         self.stop()
 
-    @discord.ui.button(label="Hit", style=discord.ButtonStyle.primary)
+    @discord.ui.button(label="Hit", style=discord.ButtonStyle.primary, emoji="👊")
     async def hit(self, interaction: discord.Interaction, _: discord.ui.Button):
-        self.playerHand.addCard(self.deck.drawCard())
-        if self.playerHand.isBust() or self.playerHand.isBlackjack():
+        self.playerHand.append(self.deck.drawCard())
+        if self.endCheck(self.playerHand):
             await self.endGame(interaction)
             return
         self.update_hands()
-        await interaction.response.edit_message(embed=self.embed, view=self)
+        await self.update(interaction)
 
-    @discord.ui.button(label="Stand", style=discord.ButtonStyle.primary)
+    @discord.ui.button(label="Stand", style=discord.ButtonStyle.blurple, emoji="👋")
     async def stand(self, interaction: discord.Interaction, _: discord.ui.Button):
-        while self.dealerHand.handValue() < 17:
-            self.dealerHand.addCard(self.deck.drawCard())
-            if self.dealerHand.isBust():
+        self.dealerHand[1].down = False
+        while self.handValue(self.dealerHand) < 17:
+            self.dealerHand.append(self.deck.drawCard())
+            if self.endCheck(self.dealerHand):
                 break
         await self.endGame(interaction)
 
