f81cda515b6a098c379816d7ff07ac31085290f8
steven santamorena <stevenasanta@gmail.com>
2024-10-29 20:51:15 -0400

    Add enrollment for starboard channel

--- cogs/starboard.py
+++ cogs/starboard.py
@@ -1,6 +1,7 @@
 import logging
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 from bot import MiniSigma
 
@@ -11,6 +12,7 @@ class StarBoard(commands.Cog):
         self.client = client
         self.db = client.db
         self.score_threshold = 1  # Maybe move this to a config file later
+        self.db.create_starboard_tables()
 
     def create_embed(self, message: discord.Message) -> discord.Embed:
         '''Creates an embed for displaying a message on the starboard'''
@@ -42,10 +44,17 @@ class StarBoard(commands.Cog):
             content=f"⭐ {self.num_stars(message)} | <#{message.channel.id}>",
             embed=self.create_embed(message)
         )
+    
+    def is_starboard_server(self, guild_id: int) -> bool:
+        '''Checks if a server has a starboard channel set'''
+        return self.db.get_starboard_channel(guild_id) is not None
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, event: discord.RawReactionActionEvent):
         if event.emoji.name != "⭐":
+            return
+        
+        if not self.is_starboard_server(event.guild_id):
             return
 
         channel = self.client.get_channel(event.channel_id)
@@ -53,8 +62,48 @@ class StarBoard(commands.Cog):
 
         logger.info(f"Registered star reaction on message '{channel}'")
 
-        # Not final implementation, simply testing in prod lol
-        await self.send_starboard_message(channel, message)
+        if self.num_stars(message) < self.score_threshold:
+            return
+        
+        channel_message_tuple = self.db.get_starboard_message(message.id)
+
+        if channel_message_tuple:
+            starboard_channel_id, starboard_message_id = channel_message_tuple
+            starboard_channel = self.client.get_channel(starboard_channel_id)
+            starboard_message = await starboard_channel.fetch_message(starboard_message_id)
+
+            await starboard_message.edit(content=f"⭐ {self.num_stars(message)} | <#{message.channel.id}>")
+
+        else:
+            starboard_channel_id = self.db.get_starboard_channel(channel.guild.id)
+            starboard_channel = self.client.get_channel(starboard_channel_id)
+
+            await self.send_starboard_message(starboard_channel, message)
+
+    @app_commands.command(name="starboard", description="Set the starboard channel for the server")
+    async def starboard(self, interaction: discord.Interaction):
+        '''Set the starboard channel for the server'''
+        if not interaction.guild:
+            await interaction.response.send_message("This command must be used in a server.", ephemeral=True)
+            return
+
+        channel = interaction.channel
+        content = f"Starboard channel set to {channel.mention}!\
+            \nAll future messages with more than {self.score_threshold} stars will be displayed here."
+
+        self.db.set_starboard_channel(interaction.guild_id, channel.id)
+        await interaction.response.send_message(content=content)
+
+        logger.info(f"Starboard channel set to {channel.name} for server {interaction.guild.name}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def starboard_reset(self, ctx: commands.Context):
+        '''DROPS ALL STARBOARD TABLES'''
+        self.db.reset_starboard_tables()
+        await ctx.send("Starboard database reset!")
+
+        logger.info(f"Starboard tables reset globally by {ctx.author.name}")
 
 async def setup(client: MiniSigma):
     await client.add_cog(StarBoard(client))
