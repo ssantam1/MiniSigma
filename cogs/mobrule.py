aa4d2c90246dc58c0412910314f91695edad1288
steven santamorena <ssantam1@binghamton.edu>
2025-01-27 15:35:51 -0500

    Add mobrule cog with command to change server icon

--- cogs/mobrule.py
+++ cogs/mobrule.py
@@ -1,21 +1,19 @@
 import logging
 import discord
-from discord.ext import commands
-from discord import app_commands
 from bot import MiniSigma
 
 logger = logging.getLogger("client.mobrule")
 
-class MobRule(commands.Cog):
+class MobRule(discord.ext.commands.Cog):
     '''Some fun utilities that allow non-admins to wreak havoc.'''
     def __init__(self, client: MiniSigma):
         self.client = client
 
-    @app_commands.command(name="icon", description="Change the server icon.")
+    @discord.app_commands.command(name="icon", description="Change the server icon.")
     async def icon(self, interaction: discord.Interaction, attachment: discord.Attachment):
         '''Change the server icon.'''
-        await interaction.response.send_message("Changing server icon... Sample: " + attachment.url, ephemeral=True)
-        
+        logger.info(f"{interaction.user.name} issued /icon, ({interaction.channel})")
+        await interaction.guild.edit(icon=await attachment.read(), reason=f"Requested by {interaction.user.name}")
     
 async def setup(client: MiniSigma):
     await client.add_cog(MobRule(client))
